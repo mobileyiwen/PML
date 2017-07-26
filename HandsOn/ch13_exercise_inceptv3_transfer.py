e9c79609a0bbfe9f08ce407f76ddeb7978bc2e89
mobileyiwen <mobileyiwen@gmail.com>
2017-07-26 17:31:42 +0800

    add ch13 transfer

---
+++ HandsOn/ch13_exercise_inceptv3_transfer.py
@@ -0,0 +1,235 @@
+
+import sys
+import tarfile
+from six.moves import urllib
+import os
+
+FLOWERS_URL = "http://download.tensorflow.org/example_images/flower_photos.tgz"
+FLOWERS_PATH = os.path.join("datasets", "flowers")
+
+width = 299
+height = 299
+channels = 3
+
+def reset_graph(seed=42):
+    tf.reset_default_graph()
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+
+
+def download_progress(count, block_size, total_size):
+    percent = count * block_size * 100 // total_size
+    sys.stdout.write("\rDownloading: {}%".format(percent))
+    sys.stdout.flush()
+
+def fetch_flowers(url=FLOWERS_URL, path=FLOWERS_PATH):
+    if os.path.exists(FLOWERS_PATH):
+        return
+    os.makedirs(path, exist_ok=True)
+    tgz_path = os.path.join(path, "flower_photos.tgz")
+    urllib.request.urlretrieve(url, tgz_path, reporthook=download_progress)
+    flowers_tgz = tarfile.open(tgz_path)
+    flowers_tgz.extractall(path=path)
+    flowers_tgz.close()
+    os.remove(tgz_path)
+
+fetch_flowers()
+
+flowers_root_path = os.path.join(FLOWERS_PATH, "flower_photos")
+flower_classes = sorted([dirname for dirname in os.listdir(flowers_root_path) 
+                            if os.path.isdir(os.path.join(flowers_root_path, dirname))])
+print(flower_classes)
+
+from collections import defaultdict
+
+image_paths = defaultdict(list)
+
+for flower_class in flower_classes:
+    image_dir = os.path.join(flowers_root_path, flower_class)
+    for filepath in os.listdir(image_dir):
+        if filepath.endswith(".jpg"):
+            image_paths[flower_class].append(os.path.join(image_dir, filepath))
+
+for paths in image_paths.values():
+    paths.sort()
+
+
+#%matplotlib inline
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+n_examples_per_class = 2
+
+for flower_class in flower_classes:
+    print("Class:", flower_class)
+    plt.figure(figsize=(10, 5))
+    for index, example_image_path in enumerate(image_paths[flower_class][:n_examples_per_class]):
+        example_image = mpimg.imread(example_image_path)[:, :, :channels]
+        plt.subplot(100+n_examples_per_class*10+index+1)
+        plt.title("{}x{}".format(example_image.shape[1], example_image.shape[0]))
+        plt.imshow(example_image)
+        plt.axis("off")
+    plt.show()
+    
+
+from scipy.misc import imresize
+import numpy as np
+
+def prepare_image(image, target_width = 299, target_height = 299, max_zoom = 0.2):
+    height = image.shape[0]
+    width = image.shape[1]
+    image_ratio = width/height
+    target_image_ratio = target_width/target_height
+    crop_vertically = image_ratio<target_image_ratio
+    crop_width = width if crop_vertically else int(height*target_image_ratio)
+    crop_height = int(width/target_image_ratio) if crop_vertically else height
+  
+    #做出一個window，大小在1／1.0~1.2
+    resize_factor = np.random.rand() * max_zoom + 1.0
+    crop_width = int(crop_width / resize_factor)
+    crop_height = int(crop_height / resize_factor)
+    
+    x0 = np.random.randint(0, width - crop_width)
+    y0 = np.random.randint(0, height - crop_height)
+    x1 = x0 + crop_width
+    y1 = y0 + crop_height
+    
+    image = image[y0:y1, x0:x1]
+    
+    #翻一下
+    if np.random.rand() < 0.5:
+        image = np.fliplr(image)
+        
+    image = imresize(image, (target_width, target_height))
+    return image.astype(np.float32)/255
+
+plt.figure(figsize=(6, 8))
+plt.imshow(example_image)
+plt.title("{}x{}".format(example_image.shape[1], example_image.shape[0]))
+plt.axis("off")
+plt.show()
+
+prepared_image = prepare_image(example_image)
+plt.figure(figsize=(8, 8))
+plt.imshow(prepared_image)
+plt.title("{}x{}".format(prepared_image.shape[1], prepared_image.shape[0]))
+plt.axis("off")
+plt.show()
+    
+rows, cols = 2, 3
+
+plt.figure(figsize=(14, 8))
+for row in range(rows):
+    for col in range(cols):
+        prepared_image = prepare_image(example_image)
+        plt.subplot(rows, cols, row * cols + col + 1)
+        plt.title("{}x{}".format(prepared_image.shape[1], prepared_image.shape[0]))
+        plt.imshow(prepared_image)
+        plt.axis("off")
+plt.show() 
+
+import tensorflow as tf
+
+def prepare_image_with_tensorflow(image, target_width = 299, target_height = 299, max_zoom = 0.2):
+    """Zooms and crops the image randomly for data augmentation."""
+
+    # First, let's find the largest bounding box with the target size ratio that fits within the image
+    image_shape = tf.cast(tf.shape(image), tf.float32)
+    height = image_shape[0]
+    width = image_shape[1]
+#    height = image.shape[0]
+#    width = image.shape[1]
+    image_ratio = width / height
+    target_image_ratio = target_width / target_height
+    crop_vertically = image_ratio < target_image_ratio
+    crop_width = tf.cond(crop_vertically,
+                         lambda: width,
+                         lambda: height * target_image_ratio)
+    crop_height = tf.cond(crop_vertically,
+                          lambda: width / target_image_ratio,
+                          lambda: height)
+#    crop_width = width if crop_vertically else int(height*target_image_ratio)
+#    crop_height = int(width/target_image_ratio) if crop_vertically else height
+
+
+    # Now let's shrink this bounding box by a random factor (dividing the dimensions by a random number
+    # between 1.0 and 1.0 + `max_zoom`.
+    resize_factor = tf.random_uniform(shape=[], minval=1.0, maxval=1.0 + max_zoom)
+    crop_width = tf.cast(crop_width / resize_factor, tf.int32)
+    crop_height = tf.cast(crop_height / resize_factor, tf.int32)
+    box_size = tf.stack([crop_height, crop_width, 3])   # 3 = number of channels
+
+    # Let's crop the image using a random bounding box of the size we computed
+    image = tf.random_crop(image, box_size)
+
+    # Let's also flip the image horizontally with 50% probability:
+    image = tf.image.random_flip_left_right(image)
+
+    # The resize_bilinear function requires a 4D tensor (a batch of images)
+    # so we need to expand the number of dimensions first:
+    image_batch = tf.expand_dims(image, 0)
+
+    # Finally, let's resize the image to the target dimensions. Note that this function
+    # returns a float32 tensor.
+    image_batch = tf.image.resize_bilinear(image_batch, [target_height, target_width])
+    image = image_batch[0] / 255  # back to a single image, and scale the colors from 0.0 to 1.0
+    return image
+
+reset_graph()
+
+input_image = tf.placeholder(tf.uint8, shape=[None, None, 3])
+prepared_image_op = prepare_image_with_tensorflow(input_image)
+
+with tf.Session():
+    prepared_image = prepared_image_op.eval(feed_dict={input_image: example_image})
+    
+plt.figure(figsize=(6, 6))
+plt.imshow(prepared_image)
+plt.title("{}x{}".format(prepared_image.shape[1], prepared_image.shape[0]))
+plt.axis("off")
+plt.show()
+
+
+from tensorflow.contrib.slim.nets import inception
+import tensorflow.contrib.slim as slim
+
+reset_graph()
+
+X = tf.placeholder(tf.float32, shape=[None, height, width, channels], name="X")
+training = tf.placeholder_with_default(False, shape=[])
+with slim.arg_scope(inception.inception_v3_arg_scope()):
+    logits, end_points = inception.inception_v3(X, num_classes=1001, is_training=training)
+
+inception_saver = tf.train.Saver()
+
+logits.op.inputs[0].op.inputs[0].op.inputs[0]
+end_points["PreLogits"]
+prelogits = tf.squeeze(end_points["PreLogits"], axis=[1, 2])   #what is this line for??
+
+n_outputs = len(flower_classes)
+
+with tf.name_scope("new_output_layer"):
+    flower_logits = tf.layers.dense(prelogits, n_outputs, name="flower_logits")
+    Y_proba = tf.nn.softmax(flower_logits, name="Y_proba")
+    
+y = tf.placeholder(tf.int32, shape=[None])
+
+with tf.name_scope("train"):
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=flower_logits, labels=y)
+    loss = tf.reduce_mean(xentropy)
+    optimizer = tf.train.AdamOptimizer()
+    flower_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="flower_logits")
+    training_op = optimizer.minimize(loss, var_list=flower_vars)
+    
+with tf.name_scope("eval"):
+    correct = tf.nn.in_top_k(flower_logits, y, 1)
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+    
+with tf.name_scope("init_and_save"):
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+    
+[v.name for v in flower_vars]
+
+     
+    
